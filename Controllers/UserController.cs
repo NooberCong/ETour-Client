ffb5b5523b777358a3820521d216c87b0b49fe13
Bear-Dev <72665227+babybear0311@users.noreply.github.com>
2021-06-02 21:44:59 +0700

    User's Information can be shown by registered account

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities;
+﻿using Client.Models;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Client.Controllers
 {
@@ -16,23 +18,47 @@ namespace Client.Controllers
         private readonly ITourRepository _tourRepository;
         private readonly ICustomerRepository _customerRepository;
         private readonly IOrderRepository _orderRepository;
-        public UserController(IOrderRepository orderRepository, ICustomerRepository customerRepository,IBookingRepository bookingRepository, ITripRepository tripRepository, ITourRepository tourRepository)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork,IOrderRepository orderRepository, ICustomerRepository customerRepository,IBookingRepository bookingRepository, ITripRepository tripRepository, ITourRepository tourRepository)
         {
+            _unitOfWork = unitOfWork;
             _orderRepository = orderRepository;
             _bookingRepository=bookingRepository;
             _tripRepository = tripRepository;
             _tourRepository = tourRepository;
             _customerRepository = customerRepository;
+            
         }
 
         // Display user main screen, including user details and list of upcoming trips
         // View(userHomeViewModel)
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            IEnumerable<Booking> objList = _bookingRepository.Queryable.Include(bk => bk.Trip).ThenInclude(tr => tr.Tour);
-            return View(objList);
+            string customerID = User.Claims.First(c1 => c1.Type == ClaimTypes.NameIdentifier).Value;
+            IEnumerable<Booking> bookingList = _bookingRepository.Queryable.Include(bk => bk.Trip).ThenInclude(tr => tr.Tour);
+            Customer customer = await _customerRepository.FindAsync(customerID);
+            return View(new UserHomeModel
+            {
+                booking = bookingList,
+                customer = customer
+            }) ;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Index(Customer cs)
+        {
+            string customerID = User.Claims.First(c1 => c1.Type == ClaimTypes.NameIdentifier).Value;
+            Customer Customer = await _customerRepository.FindAsync(customerID);
+            Customer = cs;
+            IEnumerable<Booking> bookingList = _bookingRepository.Queryable.Include(bk => bk.Trip).ThenInclude(tr => tr.Tour);
+            await _unitOfWork.CommitAsync();
+            return View(new UserHomeModel
+            {
+                booking = bookingList,
+                customer = Customer
+            });
+        }
         // Display list of tours that user followed
         // Return View(listFavoriteTours)
         public IActionResult Favourite()
@@ -42,23 +68,13 @@ namespace Client.Controllers
 
         // Display user info edit screen
         // Return View(userInfo)
-        public IActionResult UpdateInfo()
-        {
-            string customerID = User.Claims.First(c1 => c1.Type == ClaimTypes.NameIdentifier).Value;
-            //Customer customer = await _customerRepository.FindAsync(customerID);
-            return View();
-        }
-
+   
         // Action for editting user info, this action is called when a form is sent(POST) to the action
         // Param user represents the updated user instance, object is used as a placeholder class because User class is not written
         // Return View(userInfo) with update status (fail/sucess)
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult UpdateInfo(ICustomerRepository cu)
-        {   
-            
-            return View();
-        }
+       
 
         // Display trips that user has bought tickets for, including the option to review the trip (redirect to the tour details page)
         // Parameter pageNumber specify which set of trip record to display according to pageSize
---
+++ Models/User/UserHomeModel.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class UserHomeModel
+    {
+        public Customer customer { get; set; }
+        public IEnumerable<Booking> booking { get; set; }
+
+    }
+}
