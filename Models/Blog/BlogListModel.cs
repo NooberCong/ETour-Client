372a13456a18ef1442bd05b9ff8755f17508eaaf
unknown <andyduss0910@gmail.com>
2021-06-17 00:26:23 +0700

    17/6 Blog

---
+++ Models/Blog/BlogListModel.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Infrastructure.InterfaceImpls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class BlogListModel
+    {
+      
+            public IEnumerable<Post> Posts { get; set; }
+            public IPost<Employee>.PostCategory Category { get; set; }
+            public bool ShowHidden { get; set; }
+    }
+}
