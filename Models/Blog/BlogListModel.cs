5e252649fe3fdc5dd6326d817afeecc604f6aaed
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-21 23:17:08 +0700

    Blog filter and Post Detail added, Enum string underscore fix

--- Models/Blog/BlogListModel.cs
+++ Models/Blog/BlogListModel.cs
@@ -1,17 +1,13 @@
 ﻿using Core.Entities;
+using Core.Helpers;
+using Core.Value_Objects;
 using Infrastructure.InterfaceImpls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Client.Models
 {
     public class BlogListModel
     {
-      
-            public IEnumerable<Post> Posts { get; set; }
-            public IPost<Employee>.PostCategory Category { get; set; }
-            public bool ShowHidden { get; set; }
+        public PaginatedList<IPost<Employee>> Posts { get; set; }
+        public BlogFilterParams FilterParams { get; set; }
     }
 }
