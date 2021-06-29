061f6c870682ff1b2afc90c59217ad386d910de9
Bear-Dev <72665227+babybear0311@users.noreply.github.com>
2021-06-29 22:45:00 +0700

    PointLog was added

---
+++ Controllers/PointController.cs
@@ -0,0 +1,34 @@
+﻿using Client.Models;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Client.Controllers
+{
+    public class PointController : BaseController
+    {
+        private readonly ITripRepository _tripRepository;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly ICustomerRepository _customerRepository; 
+        private readonly IUnitOfWork _unitOfWork;
+        public PointController(IUnitOfWork unitOfWork, ICustomerRepository customerRepository, IBookingRepository bookingRepository, ITripRepository tripRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _bookingRepository = bookingRepository;
+            _customerRepository = customerRepository;
+            _tripRepository = tripRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+          Customer customer = await _customerRepository.FindAsync(UserID);
+            IEnumerable<PointLog> log =  _customerRepository.GetPointsLogs(customer);
+           
+            return View(log);
+        }
+    }
+}
