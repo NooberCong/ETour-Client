7cbd80fa796c9fd1fd362355a84ec9407b10f430
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-29 22:30:40 +0700

    IsOpen replaced with CanBook for trip

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,6 @@ namespace ETourClient
             services.AddScoped<TripRecommendationService>();
             services.AddScoped<BlogRecommendationService>();
             services.AddScoped<QRCodeService>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
